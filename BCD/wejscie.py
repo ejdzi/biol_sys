923a3eda80d1c3834f19be309b06c35c8e1ce244
pjanek <pj344973@students.mimuw.edu.pl>
2016-04-08 03:08:26 +0200

    Obliczanie modułów i wizualizacja

--- BCD/wejscie.py
+++ BCD/wejscie.py
@@ -3,8 +3,11 @@ from math import sqrt
 
 import networkx as nx
 
-from BCD.TreeMaker import maketree
+from TreeMaker import maketree
 
+from ete2 import Tree, TreeStyle, NodeStyle
+from random import choice
+from copy import deepcopy
 
 def add_betweenness(graf):
     b_dict = nx.edge_betweenness_centrality(graf)
@@ -50,17 +53,81 @@ def remove_edges(graf):
         add_betweenness(graf)
     return deleted_edges
 
+def color():
+    k = "#"
+    for i in range(3): k += choice(["AA","BB","CC","DD","77","88","99"])
+    return k
 
-def rysuj_drzewo(drzewo):
-    # todo Janek, weź daj tą swoją funkcję!!!
-    pass
+def find_modules_and_draw_tree(drzewo, graph):
+    t = Tree(str(drzewo)+";")
+    t.img_style["bgcolor"] = "lavender"
+    ts = TreeStyle()
+
+    special = set() #defaultdict(list)
+    for l in t.iter_leaves():
+        tmp = l
+        special.add(tmp)
+        up = True
+        while up:
+            up = False
+            if tmp.is_root():
+                break
+            else:
+                for children in tmp.up.children:
+                    if children.is_leaf():
+                        tmp = tmp.up
+                        special.add(tmp)
+                        up = True
+                        break
+    modules = set()
+    for node in t.traverse('preorder'):
+        if node in special:
+            good = True
+            for n in node.traverse('preorder'):
+                if n not in special:
+                    good = False
+                    break
+            if good:
+                if not node.is_leaf(): modules.add(node)
+                for n in node.traverse('preorder'):
+                    special.remove(n)
+
+    # below: merging modules by pre-defined cutoff
+    def check_modules(i,j,graph,cutoff):
+        for ii in i.iter_leaves():
+            for jj in j.iter_leaves():
+                if ii.name[1:-1] in graph[jj.name[1:-1]] and graph[jj.name[1:-1]][ii.name[1:-1]]["com"] > cutoff:
+                    return True
+        return False
+
+    while True:
+        breakkk = False
+        for i in modules:
+            if breakkk: break
+            for j in modules:
+                if i!=j and (not i.is_root()) and i.up == j.up and check_modules(i,j,graph, 0.1): # CUTOFF HERE
+                    breakkk = True
+                    break
+        if breakkk:
+            modules.add(i.up)
+            modules.remove(i)
+            modules.remove(j)
+        else: break # heh...
+
+    ts.show_leaf_name = True
+    for i in modules:
+        style_ca = NodeStyle()
+        style_ca["bgcolor"] = color()
+        i.set_style(style_ca)
+    t.show(tree_style=ts)
 
 
 def run(filename):
     graf = load_graph(filename)
+    graf_copy = deepcopy(graf)
     usuniete_krawedzie = remove_edges(graf)
     drzewo = maketree(usuniete_krawedzie)
-    rysuj_drzewo(drzewo)
+    find_modules_and_draw_tree(drzewo, graf_copy)
 
 
 run('l.gexf')
